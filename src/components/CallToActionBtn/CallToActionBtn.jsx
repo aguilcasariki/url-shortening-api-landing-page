c2e0af78ce09fd4e33334183f45fcf6e4e066494
Riki <aguilcasariki@gmail.com>
2023-08-05 15:51:35 -0400

    Shorten app ready

--- src/components/CallToActionBtn/CallToActionBtn.jsx
+++ src/components/CallToActionBtn/CallToActionBtn.jsx
@@ -1,7 +1,14 @@
-const defaultStyles = "bg-cyan text-white hover:bg-CTA-hover-bg";
-const CallToActionBtn = ({ styles, handleClick, label, isSubmit }) => {
+const defaultStyles = "bg-cyan text-white hover:bg-CTA-hover-bg shadow-lg";
+const CallToActionBtn = ({
+  styles,
+  handleClick,
+  label,
+  isSubmit,
+  clickAction,
+}) => {
   return (
     <button
+      onClick={clickAction}
       type={isSubmit ? "submit" : undefined}
       className={`${defaultStyles} ${styles}`}
     >
--- src/components/Shorten/ShortenCard/ShortenCard.jsx
+++ src/components/Shorten/ShortenCard/ShortenCard.jsx
@@ -1,18 +1,40 @@
 import PropTypes from "prop-types";
 import CallToActionBtn from "../../CallToActionBtn/CallToActionBtn";
+import { useState } from "react";
 
-const ShortenCard = ({ originalLink, shortenLink }) => {
+const ShortenCard = ({ originalLink, shortenUrl }) => {
+  const [isCopied, setIsCopied] = useState(false);
+  const handleClick = async () => {
+    try {
+      await navigator.clipboard.writeText(shortenUrl);
+      setIsCopied(true);
+      setTimeout(() => {
+        setIsCopied(false);
+      }, 5000);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
   return (
-    <div className="card_container flex flex-col bg-white rounded-md mt-5 py-2 md:flex-row md:justify-between md:items-center">
-      <p className="mx-3 text-xs text-very-dark-violet">
-        {originalLink}originalLink
-      </p>
+    <div className="card_container flex flex-col bg-white rounded-md mt-5 py-2 md:flex-row md:justify-between md:items-center shadow-2xl">
+      <a
+        href={originalLink}
+        className="mx-3 text-xs text-very-dark-violet whitespace-nowrap"
+      >
+        {originalLink}
+      </a>
       <hr className="w-full border-0 border-t-[1px] border-gray my-2 md:hidden" />
       <div className="shorten_link_button flex flex-col md:flex-row px-3 md:items-center gap-5">
-        <p className=" text-xs text-cyan">{shortenLink}shortenLink</p>
+        <a
+          href={shortenUrl}
+          className="text-xs text-cyan whitespace-nowrap underline hover:text-CTA-hover-bg"
+        >
+          {shortenUrl}
+        </a>
 
         <CallToActionBtn
-          label="Copy"
+          clickAction={handleClick}
+          label={isCopied ? "Copied!" : "Copy"}
           styles="w-full rounded-md  py-2 text-xs font-medium mt-2 md:m-0 md:px-5 self-center"
         />
       </div>
@@ -22,7 +44,7 @@ const ShortenCard = ({ originalLink, shortenLink }) => {
 
 ShortenCard.propTypes = {
   originalLink: PropTypes.string,
-  shortenLink: PropTypes.string,
+  shortenUrl: PropTypes.string,
 };
 
 export default ShortenCard;
